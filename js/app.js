a03549d187ed335bc909f23bd8c30f697bceff18
Sean Jun <seanjun21@gmail.com>
2016-06-21 14:00:23 -0700

    Added Game Features

--- js/app.js
+++ js/app.js
@@ -1,27 +1,52 @@
 $(document).ready(function(){
+	// DECLARE VARIABLES FOR PLAYER 1, 2, CURRENT TURN
+	var playerOne = "Player 1";
+	var playerTwo = "Player 2";
+	var currentTurn = playerOne;
 
-	var playerOne = "Player 1"
-	var playerTwo = "Player 2"
-	var currentTurn = playerOne
-
+	//DISPLAY GAMESTART STATUS (PLAYER 1'S TURN)
 	displayStatus();
 	
-$('.square').click(function(){
-	// if player 1 -- .text X
-	// if player 2 -- .text O
-		// 
-});
+	$('.square').click(function(){
+		// STOP USER FROM OVERRIDING OCCUPIED CELL
+		if ($(this).text() !== '') {
+			return;
+		} 
+		// PLACES X FOR 1, O FOR 2, THEN SWITCHES TURN
+		if (currentTurn === playerOne) {
+			$(this).text('X');
+			currentTurn = playerTwo;
+		} 
+		else {
+			$(this).text('O');
+			currentTurn = playerOne;
+		}
+		// UPDATE STATUS
+		displayStatus();
 
-// finish turn button
-// Side div displays which player's turn
+		// CHECKS IF GAME ENDS IN DRAWS
+		// if ($('#board-table td').text() !== '') {
+		// 	$('.status').text("Draw!");
+		// 	gameOver();
+		// } 
+	});
+	
+	// FUNCTION: DISPLAY PLAY AGAIN BUTTON AT THE END
+	function gameOver () {
+		$('#start-new').show();
+	}
 
-// logic for winner
-// display alert Congrats, Player X wins!
-// start new game button
+	// NEW GAME CLEARS BOARD, RESET TURN AND STATUS
+	$('#start-new').click(function() {
+		$('.square').text('');
+		currentTurn = playerOne;
+		displayStatus();
 
-function displayStatus() {
-	$('.status').text(currentTurn + "'s" + " turn");
+	});
 
-};
+	// FUNCTION: DISPLAYS CURRENT PLAYER STATUS
+	function displayStatus() {
+		$('.status').text(currentTurn + "'s" + " turn");
 
+	}
 });
